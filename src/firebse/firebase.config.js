a4bfe839fd3077ad2a75cdf9ed9c02f3bbba75e5
Hridoy-shill <ridoyaaa60@gmail.com>
2023-05-01 22:28:03 +0600

    react-router, firebase, tailwind, deisyUi installed

---
+++ src/firebse/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCi3634R7B7yFIXJH25xhDeCBGPsFwqvLs",
+  authDomain: "food-city-ede80.firebaseapp.com",
+  projectId: "food-city-ede80",
+  storageBucket: "food-city-ede80.appspot.com",
+  messagingSenderId: "189539039729",
+  appId: "1:189539039729:web:406540daec61f13b09ac50"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
