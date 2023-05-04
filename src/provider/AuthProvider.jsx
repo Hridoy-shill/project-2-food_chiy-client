fcf1c58d6b6381fce2bec3c2de885200eb239745
Hridoy-shill <ridoyaaa60@gmail.com>
2023-05-04 10:53:37 +0600

    loader added

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -8,33 +8,41 @@ const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
 
-    const [user, setUser] = useState(null)
+    const [user, setUser] = useState(null);
+    const [loading, SetLoading] = useState(true);
+
     const google = new GoogleAuthProvider();
     const gitHub = new GithubAuthProvider()
 
     const createNewUser = (email, password) => {
+        SetLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
     const logInUser = (email, password) => {
+        SetLoading(true)
         return signInWithEmailAndPassword(auth, email, password)
     }
 
     const loginWithGoogle = () => {
+        SetLoading(true)
         return signInWithPopup(auth, google);
     }
 
     const loginWithGitHub = () => {
+        SetLoading(true)
         return signInWithPopup(auth, gitHub);
     }
 
     const logOut = () => {
+        SetLoading(true)
         return signOut(auth);
     }
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, loggedUser => {
             setUser(loggedUser)
+            SetLoading(false)
         })
 
         return () => {
@@ -50,7 +58,7 @@ const AuthProvider = ({ children }) => {
         loginWithGoogle,
         loginWithGitHub,
         logOut,
-        auth
+        loading
     }
 
     return (
--- src/routes/PrivetRoute.jsx
+++ src/routes/PrivetRoute.jsx
@@ -3,13 +3,19 @@ import { AuthContext } from '../provider/AuthProvider';
 import { Navigate, useLocation } from 'react-router-dom';
 
 const PrivetRoute = ({ children }) => {
-    const { user } = useContext(AuthContext);
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
+
+    if (loading) {
+        return <div className='flex justify-center items-center h-screen'>
+            <button className="btn btn-square loading bg-white border-none text-orange-400 font-bold"></button>
+        </div>
+    }
 
     if (user) {
         return children;
     }
-    return <Navigate to={'/login'} state={{from: location}} replace></Navigate>;
+    return <Navigate to={'/login'} state={{ from: location }} replace></Navigate>;
 };
 
 export default PrivetRoute;
